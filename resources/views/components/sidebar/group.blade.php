bfb9460cd8d65d0a56ba97bd3ecff5f112af713b
daguilarm <damian.aguilarm@gmail.com>
2021-03-30 14:38:52 +0200

    refactoring

--- resources/views/components/sidebar/group.blade.php
+++ resources/views/components/sidebar/group.blade.php
@@ -1,13 +1,11 @@
 {{-- Get the group elements. See Daguilarm\Belich\App\View\Components\Group --}}
-<div
-    x-data="{ toggle: true }"
->
+<div x-data="{ toggle: true }">
 
     <a
         href="javascript:void(0);"
         class="flex items-center p-3  border-b border-gray-900 hover:text-yellow-400"
         @click="toggle = !toggle"
-        :class="$parent.isClose ? 'justify-center' : 'justify-start'"
+        :class="{ 'justify-center': $parent.isClose, 'justify-start': !$parent.isClose }"
     >
 
         {{-- Icon --}}
@@ -20,7 +18,7 @@
         {{-- Text block --}}
         <div
             class="flex ml-2"
-            :class="$parent.isClose ? 'hidden-item' : ''"
+            :class="{ 'hidden-item': $parent.isClose }"
         >
             {{-- Text --}}
             {{ $text }}
@@ -30,8 +28,8 @@
                 <svg
                     fill="currentColor"
                     viewBox="0 0 20 20"
-                    :class="{'rotate-180': !toggle, 'rotate-0': toggle}"
                     class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"
+                    :class="{'rotate-180': !toggle, 'rotate-0': toggle}"
                 >
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                 </svg>
