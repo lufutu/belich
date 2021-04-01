d2f8469352f54560a10b304054abdeef2cbc7944
daguilarm <damian.aguilarm@gmail.com>
2021-04-01 18:35:26 +0200

    updated

--- resources/views/components/sidebar/home.blade.php
+++ resources/views/components/sidebar/home.blade.php
@@ -1,13 +1,13 @@
-<div class="h-20 bg-{{ config('belich-theme.sidebar.homeLogoColor') }}-400 border-b-2 border-{{ config('belich-theme.sidebar.homeLogoColor') }}-600 flex items-center justify-center">
+<div class="w-full h-20 bg-{{ config('belich-theme.sidebar.homeLogoColor') }}-200 border-b-2 border-{{ config('belich-theme.sidebar.homeLogoColor') }}-600 flex items-center justify-center">
 
     {{-- Link --}}
     <a
-        class="block p-4 text-white text-lg"
+        class="w-full flex items-center justify-center block p-2 text-white text-lg"
         href="{{ $url }}"
     >
 
         {{-- Icon --}}
-        <span class=" bg-{{ config('belich-theme.sidebar.homeLogoColor') }}-600 hover:bg-white text-white hover:text-{{ config('belich-theme.sidebar.homeLogoColor') }}-600 rounded-full h-10 w-10 flex items-center justify-center">
+        <span class="bg-{{ config('belich-theme.sidebar.homeLogoColor') }}-600 rounded-full h-10 w-10 flex items-center justify-center">
             {{ $icon }}
         </span>
 
